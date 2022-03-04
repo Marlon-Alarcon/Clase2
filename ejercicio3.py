3d7c7a68938b43cee3ba396cfacd1f47f574b01b
Marlon-Alarcon <mralarcon@uniguajira.edu.co>
2022-03-04 09:13:30 -0500

    ejercicio
    hola a todos

--- ejercicio3.py
+++ ejercicio3.py
@@ -4,12 +4,60 @@
 ----------- Manejo de Listas ----------
 **************************************** """
 
+from re import I
+
+
 def listas():
     lista1 = []
     lista2 = list()
 
-    listaConElementos = [30, 2000000, "Marlon", "Estudiante", True, ["Magister", "Doctorado", "Especializacion", "Años de experiencia", 20]]
+    listaConElementos = [30, 2000000, "Ing Sistemas", "Marlon", "Estudiante", True, ["Magister", "Doctorado", "Especializacion", "Años de experiencia", 20]]
 
+    # print(listaConElementos[1]) # un elemento
+
+# Utilizando el ciclos
+
+    # for i in range(6): #la cantidad de elementos de la lista
+    #     print(listaConElementos[i])
+
+    # for i in range(len(listaConElementos)): #Obtiene el tamaño del arreglo
+    #     print(listaConElementos[i])
+    
+    # print("")
+    # print("")
+    # print("")
+    # print("Mostrando Elementos con ciclo while")
+
+    # j=0
+
+    # while j < len(listaConElementos):
+    #     print(listaConElementos[j]) #va j
+    #     j = j + 1
+
+    listaConElementos[1] = listaConElementos[1] + 200000 # aumentamos 200.000
+
+    print(listaConElementos[6][3]) # que me muestre el ultimo elemento forma 1
+    print(listaConElementos[-1][3]) #que me muestre el ultimo elemento
+    print(listaConElementos[0:3]) # le defino rango - muestra la posicion de la 0 hasta la 3
+#pares
+    print(listaConElementos[1:6:2])
+
+#Impares
+    print(listaConElementos[0:6:2])
+
+    # listaConElementos.append("Sede Riohacha") #agregar un elemento a lo ultimo de la lista
+    # print(listaConElementos)
+
+    # listaConElementos.append(["sede riohacha", "Miguel"]) #agregar un arreglo
+    # print(listaConElementos)
+
+    listaConElementos.insert(2, [ "Sede Riohacha", "Miguel"]) #agregar un elemento en la posicion que quiera
+    print(listaConElementos)
+
+    # del listaConElementos[2] #elimina elemento de la posicion x
+    # print(listaConElementos)
+
+    listaConElementos.remove("Estudiante")
     print(listaConElementos)
 
 def main():
---
+++ ejercicio4.py
@@ -0,0 +1,12 @@
+from api.library import *
+
+def main():
+    #Aplicacion de los diccionarios
+
+    persona = { # dos objetos
+        "nombre": "Marlon",
+        "apellidos": "alarcon",
+        "edad": 22
+    }
+
+    
