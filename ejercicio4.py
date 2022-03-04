57af9a0b9130165d119e78aadf239938a9ee3d4b
Marlon-Alarcon <mralarcon@uniguajira.edu.co>
2022-03-04 09:15:24 -0500

    Update ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,12 +1,2 @@
-from api.library import *
-
-def main():
-    #Aplicacion de los diccionarios
-
-    persona = { # dos objetos
-        "nombre": "Marlon",
-        "apellidos": "alarcon",
-        "edad": 22
-    }
 
     
