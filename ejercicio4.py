0eb7b1a5e972e3d4cf65e70c320837c591e3b1bf
Marlon-Alarcon <mralarcon@uniguajira.edu.co>
2022-03-09 09:14:16 -0500

    Modificar

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,20 +1,34 @@
+from api.library import *
 
-persona = {
-    "datospersonales": {
+def main():
+    #aplicacion de los diccionarios
+
+    # persona = {
+    #     "nombre": "Geraldine",
+    #     "apellidos": "Suarez Solano",
+    #     "edad": 22
+    # }
+    #print (persona["nombre"]+" "+ persona["apellidos"])
+
+    # persona ["nombre"] = "Karina"  ---> Cambiar nombre
+    # print (f"{persona['nombre']} {persona['apellidos']}")
+
+    persona = { #este objeto, tiene dos objetos
+        "datos_personales":{
         "nombre": "Marlon",
-        "apellido": "Alarcon",
-        "edad": 22,
-    },
+        "apellidos": "Alarcon Bonivento",
+        "edad": 22
+        },
 
-    "salarial": {
-        "salario": 2000000,
-        "subtranporte": 50000,
-        "subalimentacion": 60000
+        "salarial": {
+            "salario": 2000000,
+            "subtransporte": 50000,
+            "subalimentacion": 60000
+        }
+        
     }
-}
-
-print(persona["salarial"])
-print (f"Nombre: {persona ['datospersonales']['nombre']})
+    print(persona["salarial"])
+    print (f"Nombre: {persona ['datos_personales']['nombre']} {persona ['datos_personales']['apellidos']}")
 
 if __name__ == "__main__":
     main()
